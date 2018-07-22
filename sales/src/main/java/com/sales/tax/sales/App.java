39166df67943e77d531640c55681401d16288fe4
caiorodrig0 <41527929+caiorodrig0@users.noreply.github.com>
2018-07-21 22:27:11 -0300

    Initial Commit
    First commit of the application.

---
+++ sales/src/main/java/com/sales/tax/sales/App.java
@@ -0,0 +1,13 @@
+package com.sales.tax.sales;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
