021e0ad58e48916c85f80e4d9e8696e9f762d5d0
caiorodrig0 <caio.cordeiro@mail.com>
2018-07-23 16:02:38 -0300

    syncrhonzing branch

--- sales/src/main/java/com/sales/tax/sales/src/main/java/com/sales/tax/service/SalesService.java
+++
@@ -1,8 +0,0 @@
-package com.sales.tax.service;
-
-import com.mongodb.MongoWriteException;
-
-public interface SalesService {
-
-	void addProduct(String item, Integer type, Boolean isImported) throws MongoWriteException;
-}
--- sales/src/main/java/com/sales/tax/sales/src/main/java/com/sales/tax/service/exception/ProductAlreadyExistsException.java
+++
@@ -1,5 +0,0 @@
-package com.sales.tax.service.exception;
-
-public class ProductAlreadyExistsException {
-
-}
